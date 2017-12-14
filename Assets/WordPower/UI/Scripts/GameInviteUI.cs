24b1e76be455b774fd300b6d45d0d5d4dd386674
Intaj <intajcse@gmail.com>
2017-12-15 00:44:33 +0530

    GameInvite code update.

--- Assets/WordPower/UI/Scripts/GameInviteUI.cs
+++ Assets/WordPower/UI/Scripts/GameInviteUI.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameInviteUI : MonoBehaviour {
+public class GameInviteUI : MonoBehaviour
+{
 	UIManager uiManager;
 	GameManager gameManager;
-	void Start()
+
+	void Start ()
 	{
 		uiManager = UIManager.instance;
 		gameManager = GameManager.instace;
 	}
-	public void PlayNow(bool isPlay)
+
+	public void PlayNow (bool isPlay)
 	{
 		gameObject.SetActive (false);
 		if (isPlay) {
-			if (gameManager.availableCoin > gameManager.tablePrice) {
-				uiManager.DesableAllPanels ();
-				uiManager.roomPanel.SetActive (true);
-				uiManager.roomPanel.GetComponent<RoomUI> ().CreateRoomBySerevr ();
-				ConnectionManager.Instance.IacceptChallage ();
-			}
-		} else {
-			uiManager.storePanel.SetActive (true);
+			//if (gameManager.availableCoin > gameManager.tablePrice) {
+			uiManager.DesableAllPanels ();
+			uiManager.roomPanel.SetActive (true);
+			uiManager.roomPanel.GetComponent<RoomUI> ().CreateRoomBySerevr ();
+			ConnectionManager.Instance.IacceptChallage ();
 		}
+//		} else {
+//			uiManager.storePanel.SetActive (true);
+//		}
 	}
 
 }
